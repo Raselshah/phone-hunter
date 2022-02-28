852b7d73c20f42b35ebd835fb8cddc5d71ab18ce
Md Rasel Shah <mdraselshah56@gmail.com>
2022-02-28 22:46:30 +0600

    ui phone set

--- phone.js
+++ phone.js
@@ -1,7 +1,39 @@
+const allPhoneAdd = document.getElementById("phone-add");
+
 const searchButton = () => {
   const inputField = document.getElementById("input-field");
-  const input = inputField.value;
-  console.log(input);
+  const searchInput = inputField.value.toLowerCase();
+
+  fetch(`https://openapi.programming-hero.com/api/phones?search=${searchInput}`)
+    .then((res) => res.json())
+    .then((data) => showPhone(data.data));
 
   inputField.value = "";
+};
+
+const showPhone = (phones) => {
+  for (const phone of phones) {
+    // console.log(phone);
+
+    const div = document.createElement("div");
+    div.classList.add("col-lg-3");
+    div.classList.add("col-md-6");
+    div.classList.add("mb-4");
+    div.innerHTML = `
+    
+        <div class="col">
+            <div class="card h-100">
+                <img class="mx-auto" width="200" height="200" src="${phone.image}" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand : ${phone.brand}</p>
+                    <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-outline-info">See Phone Details</button>
+                </div>
+            </div>
+        </div>
+      
+    `;
+
+    allPhoneAdd.appendChild(div);
+  }
 };
