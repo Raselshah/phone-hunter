74e8b83ff4090b8e85ea8193d7a6d0df7169a004
Md Rasel Shah <mdraselshah56@gmail.com>
2022-03-01 00:26:32 +0600

    phone details added

--- phone.js
+++ phone.js
@@ -1,6 +1,9 @@
 const allPhoneAdd = document.getElementById("phone-add");
 // load phones function
 const searchButton = () => {
+  // clear display
+  allPhoneAdd.textContent = "";
+  // phone data load
   const inputField = document.getElementById("input-field");
   const searchInput = inputField.value.toLowerCase();
 
@@ -13,6 +16,12 @@ const searchButton = () => {
 
 // show phone value function
 const showPhone = (phones) => {
+  // user input checking
+  if (phones <= 0) {
+    alert("error");
+    return;
+  }
+  //   user input valid then this code running
   for (const phone of phones) {
     // console.log(phone);
 
@@ -48,5 +57,41 @@ const phoneDetails = (info) => {
 };
 
 const showPhoneDetails = (phoneInfo) => {
-  console.log(phoneInfo);
+  // clear display
+  allPhoneAdd.textContent = "";
+
+  console.log(phoneInfo.mainFeatures);
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h4></h4>
+
+    <div class="card">
+    <img class="mx-auto" width="200" height="200" src="${phoneInfo.image}" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">memory : ${phoneInfo.mainFeatures.memory}</h5>
+      <h5 class="card-title">Storage : ${phoneInfo.mainFeatures.storage}</h5>
+      <h5 class="card-title">body : ${phoneInfo.mainFeatures.displaySize}</h5>
+      <h5 class="card-title">Storage : ${phoneInfo.mainFeatures.chipSet}</h5>
+      <h5 class="card-title">display : ${phoneInfo.mainFeatures.displaySize}</h5>
+      <p class="card-text">bluetooth : ${phoneInfo.others.Bluetooth}</p>
+      <p class="card-text">Gps : ${phoneInfo.others.GPS}</p>
+      <p class="card-text">NFC : ${phoneInfo.others.NFC}</p>
+      <p class="card-text">Radio : ${phoneInfo.others.Radio}</p>
+      <p class="card-text">USB : ${phoneInfo.others.USB}</p>
+      <p class="card-text">WLAN : ${phoneInfo.others.WLAN}</p>
+      <p class="card-text"><small class="text-muted">${phoneInfo.releaseDate}</small></p>
+    </div>
+  </div>
+  `;
+  allPhoneAdd.appendChild(div);
+
+  const p = phoneInfo.mainFeatures.sensors;
+  for (const i of p) {
+    console.log(i);
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <p>${i}</p>
+    `;
+    allPhoneAdd.appendChild(div);
+  }
 };
